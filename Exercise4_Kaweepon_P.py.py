43c528a07b14612e1aa6db1bb7891e479a5de5bc
Kaweepon Pherngpradech <52350287+KaweeponP@users.noreply.github.com>
2019-08-12 21:28:12 +0700

    Add files via upload

---
+++ Exercise4_Kaweepon_P.py.py
@@ -0,0 +1,13 @@
+f="Foundation English:"
+FE = 89
+g = "General Business:"
+GB = 66
+i = "Introduction to Computer systems:"
+ICS = 75
+C = "Computer Programming:"
+CR = 63
+print("-----your Score-----")
+print(f, FE)
+print(g, GB)
+print(i, ICS)
+print(C, CR)
